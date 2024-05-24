d3438619131e8fd9d0c52c04537385db476323eb
Vimmi <viniciusmbarbosa2014@gmail.com>
2024-05-23 21:50:47 -0300

    Atualizando lista tefônica

--- ListaTelefonica.sh
+++ ListaTelefonica.sh
@@ -4,17 +4,16 @@
 
 function inicio(){
 
-local cam=""
-#read -e -p "Digite um caminho para o arquivo " cam
-cam="/home/vimmi/ScriptsBash/"
-#testa até a variavel cam ter um caminho valido
-while [[ ! -d "$cam" ]]; do 
+    cam=""
     read -e -p "Digite um caminho para o arquivo " cam
-done
-arq="${cam}lista.txt"
-#testa se o arquivo de dados já existe, senão, cria ele, se sim não faz nada.
-[[ -e $arq ]] && echo "Os dados serão adicionados no arquivo que já existe " || echo "Criando o arquivo de dados" 
->> $arq  
+    #testa até a variavel cam ter um caminho valido
+    while [[ ! -d "$cam" ]]; do 
+        read -e -p "Digite um caminho para o arquivo " cam
+    done
+    arq="${cam}lista.txt"
+    #testa se o arquivo de dados já existe, senão, cria ele, se sim não faz nada.
+    [[ -e $arq ]] && echo "Os dados serão adicionados no arquivo que já existe " || echo "Criando o arquivo de dados" 
+    >> $arq  
 }
 
 function geraId(){
@@ -24,22 +23,23 @@ function geraId(){
 
 function addCont(){
     clear
-declare -A Cont
+    declare -A Cont
 
-for i in "Id" "Nome" "Telefone" "CPF"; do
-    if [[ $i = "Id" ]]; then
-        Cont[$i]=$(geraId)
-        else
-            read -p "Qual o ${i} do contato?" Cont[$i]
-            #Enquanto a cariavel estiver vazia, vai pedir para o useuario a preeencher
-            while [[ -z ${Cont[$i]} ]]; do 
+    for i in "Id" "Nome" "Telefone" "CPF"; do
+        if [[ $i = "Id" ]]; then
+            Cont[$i]=$(geraId)
+            else
                 read -p "Qual o ${i} do contato?" Cont[$i]
-            done
-    fi
-done
-
-echo ${Cont[Id]}¨${Cont[Nome]}¨${Cont[Telefone]}¨${Cont[CPF]} >> $arq
-[[ $? -ne 0 ]] && echo "Algo deu errado ao adicionar o Contato, Saindo..." exit 1
+                #Enquanto a cariavel estiver vazia, vai pedir para o useuario a preeencher
+                while [[ -z ${Cont[$i]} ]]; do 
+                    read -p "Qual o ${i} do contato?" Cont[$i]
+                done
+        fi
+    done
+    #Salva os dados trocando todas as letras por minusculas(para facilitar manipulação dos dados)
+    echo ${Cont[Id]} ${Cont[Nome]} ${Cont[Telefone]} ${Cont[CPF]} | tr 'A-Z' 'a-z' >> $arq 
+    #Se por algum motivo, a gravação dos dados não funcionar, para o programa.
+    [[ $? -ne 0 ]] && echo "Algo deu errado ao adicionar o Contato, Saindo..." exit 1
 }
 
 function ConsCont(){
@@ -48,30 +48,64 @@ function ConsCont(){
     opcoes=('Por nome' 'Por ID' 'Voltar ao menu')
     select opc in "${opcoes[@]}"; do
         case $REPLY in
-            1)
-                echo "Opção por nome"
+            1) 
+                read -p "Quem você deseja buscar? " pesq
+                while [[ "$pesq" =~ [1-9] ]]; do ##Testa se na String tem numeros
+                    clear
+                    echo "Nome possui apenas letras, digite novamente"
+                    read -p "Qual Id você deseja buscar? " pesq
+                done
+                cat $arq | grep "$pesq"
+                #Se o termo buscado pelo user não existir, a mensagem é exibida
+                [[ $? -ne 0 ]] && echo "Contato não existe" 
+                read -p "Aperte enter para retornar ao menu de busca"
+                ConsCont
                 ;;
-            2)  
-                echo "Opção por ID"
+            2)
+                read -p "Qual Id você deseja buscar? " pesq  
+
+                while [[ "$pesq" =~ [a-zA-Z] ]]; do ##Testa se na String tem letras
+                    clear
+                    echo "Atenção o id possui apenas numeros, digite novamente"
+                    read -p "Qual Id você deseja buscar? " pesq
+                done
+                cat $arq | grep "^$pesq"
+                #Se o termo buscado pelo user não existir, a mensagem é exibida
+                [[ $? -ne 0 ]] && echo "Id não existe" 
+                read -p "Aperte enter para retornar ao menu de busca"
+                ConsCont
                 ;;
             3)
                 echo "Voltando ao menu"
                 Menu
                 ;;
             *)
+                ConsCont
                 echo "Opção inválida"
                 ;;
         esac
     done
 }
 
+function RemCont(){
+    read -p "Diga qual o id do contato que deseja remover" id
+    while [[ "$id" =~ [a-zA-Z] ]]; do ##Testa se na String tem letras
+                    clear
+                    echo "Atenção o id possui apenas numeros, digite novamente"
+                    read -p "Qual Id você deseja remover? " id
+                done
 
+    sed "${id}d" $arq > temp.txt && mv  $arq && echo "Removido com sucesso" || echo "algo deu errado an remoção do contato"
+    echo $cam
+    read
+
+}
 
 function Menu(){
     clear
 
     PS3="Olá! O que deseja realizar?"
-    opcoes=('Adicionar contato' 'Consultar Contato' 'SAIR')
+    opcoes=('Adicionar contato' 'Consultar Contato' 'Remover Contato' 'SAIR')
     select opc in "${opcoes[@]}"; do
         case $REPLY in
             1)
@@ -83,7 +117,14 @@ function Menu(){
                 Menu
                 ;;
             3)
+                RemCont
+                Menu
+                ;;
+            3)
+                clear
                 echo "SAINDO"
+                sleep 1
+                clear
                 exit 0
                 ;;
             *)
@@ -99,4 +140,5 @@ inicio
 sleep 2
 Menu
 
-#Fazer Consulta de contatos
+#Arrumar a função de remover contato, mexi na função de Inicio
+#Arrumar função de inicio, para testar se o caminho do arquivo está certo
